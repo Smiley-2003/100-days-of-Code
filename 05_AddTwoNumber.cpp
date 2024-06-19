b29f387d39ed24cb88ef1e9d3c25c60bced705d0
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-06-20 02:03:44 +0530

    Add files via upload

---
+++ 05_AddTwoNumber.cpp
@@ -0,0 +1,22 @@
+// You are using GCC
+// C++ Program to Add Two numbers
+// Name: Naina Mogha
+// Date: 20 June 2024
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int first_number,second_number,sum;
+    
+    cout<<"Enter two integer: ";
+    cin>>first_number>>second_number;
+    
+    // Sum of two numbers is stored in
+    // variable sum of two numbers
+    sum = first_number + second_number;
+    
+    cout<<"\nsum = "<<first_number <<"+"<<second_number<<"="<<sum;
+   
+    return 0;
+}
