2085fe2d07bc7de80bf586954e147e436c9f9f97
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-06-20 02:13:20 +0530

    Add files via upload

---
+++ 07_varSize.cpp
@@ -0,0 +1,16 @@
+// You are using GCC
+// C++ Program to Find Size of int,float,double and char in your system
+// Name: Naina Mogha
+// Date: 19 June 2024
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+        cout<<"Size of char: "<<sizeof(char)<<" byte"<<endl;
+        cout<<"Size of int:"<<sizeof(int)<<" bytes"<<endl;
+        cout<<"Size of float:"<<sizeof(float)<<" bytes"<<endl;
+        cout<<"Size of double:"<<sizeof(double)<<" bytes"<<endl;
+        
+        return 0;
+}
