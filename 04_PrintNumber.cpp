78092073455419bc6703d0aa1aa4e11725e64548
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-06-20 01:52:34 +0530

    Add files via upload

---
+++ 04_PrintNumber.cpp
@@ -0,0 +1,14 @@
+// You are using GCC
+// C++ Program to Print number entered by User
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    
+    cout<<"Enter an integer:";
+    cin>>number;
+    
+    cout<<"\nYou entered "<<number;
+    return 0;
+}
