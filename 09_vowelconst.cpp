10ef7ec3e0a81cb1f49d9cb34630a63492774545
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-06-20 19:55:03 +0530

    Add files via upload

---
+++ 09_vowelconst.cpp
@@ -0,0 +1,21 @@
+// You are using GCC
+// Check whether Number is Even or Odd using if else
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    
+    cout<<"Enter an integer: \n ";
+    cin>>n;
+    
+    ( n % 2 == 0 ) ? cout<<n<<" is even ." : cout<< n << " is odd. ";
+       
+       
+    return 0;
+}
+
+
+
+
