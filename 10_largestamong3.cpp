ac5ed4b54c2083251ccce41ed6d5befc136ce31e
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-06-20 21:37:48 +0530

    Add files via upload

---
+++ 10_largestamong3.cpp
@@ -0,0 +1,26 @@
+// C++ Program to Find Largest Number Among Three Numbers
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    double n1,n2,n3;
+    
+    cout<<"Enter three numbers: ";
+    cin>>n1>>n2>>n3;
+    
+    // check if n1 is th elargest numbers
+    if(n1 >= n2 && n1 >= n3)
+        cout<<"Largest number: "<<n1;
+        
+    // check if n2 is the largest number
+    else if(n2 >= n1 && n2 >= n3)
+        cout<<"\nLargest number: "<<n2;
+        
+    // if neither n1 nor n2 are the largest,n3 is the largest
+    else
+      cout<<"\nLargest number: "<<n3;
+      
+      
+      return 0;
+}
