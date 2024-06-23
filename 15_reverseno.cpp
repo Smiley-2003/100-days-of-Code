85e942b2109bc06940c6331d820751debb5cb559
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-06-23 13:28:20 +0530

    Add files via upload

---
+++ 15_reverseno.cpp
@@ -0,0 +1,30 @@
+// Reverse a given number
+// Author: Naina Mogha
+// Date: 23 June 2024
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n; //integer is n
+    cin>>n;
+    
+    int reverse;
+    while(n>0){
+        
+        // 43 % 10 = 3
+        int lastdigit = n % 10;
+        // reverse is 0 
+        // 0 * 10 + 4 = 4
+        // 4 * 10 + 3 = 43
+        // 43 * 10 + 2 = 432
+        // 432 * 10  + 1 = 4321 
+        reverse = reverse * 10 + lastdigit;
+        // 432 % 10 = 43.2 but n is integer so  its 43
+        n = n / 10;
+
+    // print reverse of number
+    cout<<reverse<<endl;
+   }
+
+return 0;
+}
