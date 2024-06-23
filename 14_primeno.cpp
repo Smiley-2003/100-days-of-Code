625d8d659f2f5f63eda429b51e1fc05381988044
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-06-23 12:45:41 +0530

    14_primeno.cpp

--- 14_primeno.cpp
+++ 14_primeno.cpp
@@ -1,1 +1,28 @@
 // You are using GCC
+// You are using GCC
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+
+    bool flag = 0;
+    
+    for(int i = 0; i< sqrt(n); i++) {
+        if( n % 2 == 0){
+            cout<<"Non - prime"<<endl;
+            flag = 1;
+            break;
+        }
+        
+    }
+    if(flag == 0){
+        cout<<"prime"<<endl;
+    }
+    
+    return 0;
+}
