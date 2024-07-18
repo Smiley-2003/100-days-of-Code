32416f882cb4807d95ffd5512a7ac965ccbca0bd
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-07-18 17:27:30 +0530

    Add files via upload

---
+++ 17_Fibonnaci_Functions.cpp
@@ -0,0 +1,31 @@
+// You are using GCC
+//Functions Question
+//Fibonacci Sequence
+//Author: Naina Mogha
+//Date: 18 July 2024
+
+#include<iostream>
+using namespace std;
+
+void fib(int n){
+    
+   int t1 = 0;
+   int t2 = 1;
+   int nextTerm;
+   
+   for(int i=1;i<=n;i++){
+       cout<<t1<<endl;
+       nextTerm = t1+t2;
+       t1 = t2;
+       t2 = nextTerm;
+   }
+   
+   int main(){
+       
+       int n;
+       cin>>n;
+       fib(n);
+   
+   
+    return 0;
+}
