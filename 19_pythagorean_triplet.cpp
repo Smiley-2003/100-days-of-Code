b012d8090f3318d832fffe3cab2c1d2d2427a686
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-07-20 16:58:32 +0530

    Update 19_pythagorean_triplet.cpp

--- 19_pythagorean_triplet.cpp
+++ 19_pythagorean_triplet.cpp
@@ -1,3 +1,7 @@
+//Pythgorean triplet
+//Author: Naina Mogha
+//Functions questions
+
 //Sum of n natural numbers
 //Author: Naina Mogha
 //Functions questions
@@ -5,16 +9,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int sum(int n)
-{
-    int ans = 0;
-    for(int i = 1; i<= n; i++)
-            ans+=i;
-    return ans;
+bool check(int x,int y,int z){
+    int a = max(x,max(y,z));
+    int b,c;
+    
+    if(a==x)
+    {
+        b=y;
+        c=z;
+    }else if(a==y){
+        b = x;
+        c = z;
+    }
+    else{
+        b = x;
+        c = y;
+    }
+    
+    if(a*a== b*b+c*c)
+          return true;
+    else
+          return false;
 }
 
 int main(){
-    int n;
-    cin>>n;
-    cout<<sum(n)<<endl;
+    int x,y,z;
+    cin>>x>>y>>z;
+    
+    if(check(x,y,z)){
+        cout<<"Pythagorean triplet";
+        return true;
+    }
+    else{
+        cout<<"Not a pythagorean triplet!";
+        return false;
+    }
+    
+    return 0;
 }
