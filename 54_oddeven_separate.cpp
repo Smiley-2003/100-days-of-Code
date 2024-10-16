c23409436063fafecaf6766cce22b0c71ed90444
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-10-16 19:21:23 +0530

    Update 54_oddeven_separate.cpp

--- 54_oddeven_separate.cpp
+++ 54_oddeven_separate.cpp
@@ -1,3 +1,53 @@
+///// #include<vector>
+// // #include<iostream>
+// // #include<algorithm>
+// // using namespace std;
+
+// // int main(){
+// //     int T;//number of test cases
+// //     cin>>T;
+    
+// //     while(T--){
+// //         int N;//size of the array
+// //         cin>>N;
+// //         vector<int> nums(N);
+        
+// //         //reading the input array
+// //         for(int i = 0; i < N; i++){
+// //             cin>>nums[i];
+// //         }
+        
+// //         vector<int> odd,even;
+        
+// //         //separate odd and even numbers
+// //         for(int i = 0; i < N;i++){
+// //             if(nums[i] % 2==0){
+// //                 even.push_back(nums[i]);
+// //             }else{
+// //                 odd.push_back(nums[i]);
+// //             }
+            
+// //             //sort odd numbers in non-increasing order
+// //             sort(odd.begin(),odd.end(),greater<int>());
+            
+// //             //sort even numbers in non-decreasing order
+// //             sort(even.begin(),even.end());
+            
+// //             //Output the sorted array
+// //             for(int i = 0; i< odd.size();i++){
+// //                 cout<<odd[i]<<" ";
+// //             }
+            
+// //  in           for(int i = 0; i< even.size();i++){
+// //                 cout<<even[i]<<" ";
+// //             }
+// //             cout<<endl;
+// //         }
+// //         return 0;
+// //     }
+// // }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
