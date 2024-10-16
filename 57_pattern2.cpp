e7ff980f6ec154eeec1d97ecc8cd3414612e3e82
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-10-16 19:20:23 +0530

    Create 57_pattern2.cpp

---
+++ 57_pattern2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern4(int N)
+{
+    for(int i = 1; i <= N;i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int N = 5;
+    pattern4(N);
+    return 0;
+}
