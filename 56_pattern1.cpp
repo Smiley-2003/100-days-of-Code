1204a48de3f3127bd88918c45ab4cf09ffd9c3ef
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-10-16 19:19:21 +0530

    Create 56_pattern1.cpp

---
+++ 56_pattern1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern3(int N)
+{
+    for(int i = 1; i <= N;i++)
+    {
+        for(int j = 1; j <=i; j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
